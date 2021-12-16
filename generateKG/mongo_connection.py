a3425b4d1ba63ac4863dd01cf2e869c7c53472fb
854342681 <62539940+854342681@users.noreply.github.com>
2021-12-16 16:38:46 +0800

    添加注释

--- generateKG/mongo_connection.py
+++ generateKG/mongo_connection.py
@@ -10,6 +10,9 @@ myclient = pymongo.MongoClient("mongodb://222.197.219.4:27017/")
 mydb = myclient["db_base"]
 mycol = mydb["db_lan"]
 
+'''
+读取文件，导入数据
+'''
 path = ['../db100k/wikidata_erro.json','../db100k/wikidata_addition.json']
 insert = []   # 输入数据的集合
 count = len(insert)   # 计数
@@ -19,18 +22,21 @@ for i in path:
         for line in f:  # 文件过大，只能逐行打开文件
             try:
                 data = json.loads(line)
-                insert.append(data)
+                # insert.append(data)
             except Exception as e:  # 如果出错，则找到Qid，记录下来。如没有Qid就直接舍弃这段数据
                 name = re.search(r'Q\d*', line)
                 try:
                     fail.append(name.group(0))
                 except:
                     pass
-                print(e)
-        if count == 1000:   # 性能原因无法在内存中存储过多的数据，每存1000个上传一次并清空集合
-            result = mycol.insert_many(insert)
-            insert = []
-            print('done')
+                # print(e)
+        # if count == 1000:   # 性能原因无法在内存中存储过多的数据，每存1000个上传一次并清空集合
+        #     result = mycol.insert_many(insert)
+        #     insert = []
+        #     print('done')
+with open('../db100k/fail.txt','w',encoding='utf8') as f:
+    for i in fail:
+        f.write(i)
 print('all done')
 
 
