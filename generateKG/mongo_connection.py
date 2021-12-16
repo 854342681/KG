4a1ee4cd56912f9e78cfc0c98411821e89256471
854342681 <62539940+854342681@users.noreply.github.com>
2021-12-16 16:24:48 +0800

    添加注释

--- generateKG/mongo_connection.py
+++ generateKG/mongo_connection.py
@@ -3,28 +3,31 @@ import os
 import pymongo
 import re
 
-
+'''
+连接数据库，创建collection
+'''
 myclient = pymongo.MongoClient("mongodb://222.197.219.4:27017/")
 mydb = myclient["db_base"]
 mycol = mydb["db_lan"]
+
 path = ['../db100k/wikidata_erro.json','../db100k/wikidata_addition.json']
-insert = []
-count = len(insert)
-fail = []
+insert = []   # 输入数据的集合
+count = len(insert)   # 计数
+fail = []   # 失败数据集合
 for i in path:
     with open(i,'r',encoding='utf8') as f:
-        for line in f:
+        for line in f:  # 文件过大，只能逐行打开文件
             try:
                 data = json.loads(line)
                 insert.append(data)
-            except Exception as e:
+            except Exception as e:  # 如果出错，则找到Qid，记录下来。如没有Qid就直接舍弃这段数据
                 name = re.search(r'Q\d*', line)
                 try:
                     fail.append(name.group(0))
                 except:
                     pass
                 print(e)
-        if count == 1000:
+        if count == 1000:   # 性能原因无法在内存中存储过多的数据，每存1000个上传一次并清空集合
             result = mycol.insert_many(insert)
             insert = []
             print('done')
